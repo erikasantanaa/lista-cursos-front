9b7481f46cd48405992e0cad3e7f511ea521053a
erikasantanaa <erikamariasantanaa@gmail.com>
2023-04-24 21:56:43 -0300

    incluindo colunas na tabela

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../models/cursos/model/curso';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  styleUrls: ['./cursos.component.scss'],
 })
-export class CursosComponent {
 
+export class CursosComponent implements OnInit {
+  cursos: Curso[] = [
+    { _id: '1', nome: 'Erika', categoria: 'Angular'}
+  ];
+
+  colunasExibidas: ['nome', 'categoria'];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
